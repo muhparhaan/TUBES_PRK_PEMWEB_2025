b1a256ac6dddef6ce89ff267f744be15ba295e81
Febby Yolanda putri <febbyyolandaputri6@gmail.com>
2025-12-05 19:49:33 +0700

    menambah logic create(tambah data) supplier

--- kelompok/kelompok_27/src/proses/supplier_proses.php
+++ kelompok/kelompok_27/src/proses/supplier_proses.php
@@ -9,6 +9,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id_supplier = $_POST['id_supplier'] ?? 0;
     
     switch ($action) {
+        case 'tambah':
+            tambahSupplier($koneksi, $nama_supplier, $no_hp, $kategori);
+            break;
+        case 'ubah':
+            break;
         default:
             header("Location: ../admin/master_supplier.php?status=error&pesan=Aksi tidak dikenal");
             exit();
@@ -19,7 +24,20 @@ if (isset($_GET['action']) && $_GET['action'] === 'arsip' && isset($_GET['id']))
 }
 
 
-function tambahSupplier($koneksi, $nama, $hp, $kat) {  }
+function tambahSupplier($koneksi, $nama_supplier, $no_hp, $kategori) {
+    $stmt = $koneksi->prepare("INSERT INTO suppliers (nama_supplier, no_hp, kategori) VALUES (?, ?, ?)");
+    
+    $stmt->bind_param("sss", $nama_supplier, $no_hp, $kategori); 
+    
+    if ($stmt->execute()) {
+        header("Location: ../admin/master_supplier.php?status=sukses&pesan=Supplier berhasil ditambahkan!");
+    } else {
+        header("Location: ../admin/master_supplier.php?status=error&pesan=Gagal menambahkan Supplier: " . $stmt->error);
+    }
+    $stmt->close();
+    exit();
+}
+
 function ubahSupplier($koneksi, $id, $nama, $hp, $kat) { }
 function arsipSupplier($koneksi, $id) { }
 
