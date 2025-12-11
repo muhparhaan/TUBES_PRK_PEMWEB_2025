03d61f3794e452aaf37b7dca54ada893c17eaab9
Parhan <varhunts@gmail.com>
2025-12-11 16:50:57 +0700

    add timezone database

--- kelompok/kelompok_27/src/config/koneksi.php
+++ kelompok/kelompok_27/src/config/koneksi.php
@@ -9,4 +9,7 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die("Koneksi Database Gagal: " . mysqli_connect_error());
 }
+
+date_default_timezone_set('Asia/Jakarta');
+mysqli_query($conn, "SET time_zone = '+07:00'");
 ?>
